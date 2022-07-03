ce9e336cebe1d10eb92e1ca9c389e622cb3ed4e6
Outer Cloud Studio <outercloudstudio@gmail.com>
2022-07-03 15:44:18 -0500

    Particle Registry

--- src/main/java/com/outercloud/scribe/Scribe.java
+++ src/main/java/com/outercloud/scribe/Scribe.java
@@ -1,79 +1,47 @@
 package com.outercloud.scribe;
 
-import com.google.gson.Gson;
-import com.outercloud.scribe.particles.DataDrivenParticle;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleFactory;
 import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Map;
+import java.util.Hashtable;
 
 public class Scribe implements ModInitializer, ClientModInitializer {
-	public static final String NAMESPACE = "scribe";
+	private static final String NAMESPACE = "scribe";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger("scribe");
+	private static final Logger LOGGER = LoggerFactory.getLogger("scribe");
+
+	public static final DefaultParticleType PARTICLE = FabricParticleTypes.simple();
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing Scribe!");
-
-		RegisterParticles();
-	}
-
-	public static final DefaultParticleType DATA_DRIVEN_PARTICLE = FabricParticleTypes.simple();
-
-	public static void RegisterParticles() {
-		Registry.register(Registry.PARTICLE_TYPE, new Identifier(NAMESPACE, "data_driven_particle"), DATA_DRIVEN_PARTICLE);
-
-		try {
-			File directoryPath = new File("./");
-
-			File filesList[] = directoryPath.listFiles();
-
-			for(File file : filesList) {
-				System.out.println("File name: "+file.getName());
-				System.out.println("File path: "+file.getAbsolutePath());
-				System.out.println("Size :"+file.getTotalSpace());
-				System.out.println(" ");
-			}
-
-			// create Gson instance
-			Gson gson = new Gson();
-
-			// create a reader
-			Reader reader = Files.newBufferedReader(Paths.get("./scribe/mason.json"));
-
-			// convert JSON file to map
-			Map<?, ?> map = gson.fromJson(reader, Map.class);
-
-			// print map entries
-			for (Map.Entry<?, ?> entry : map.entrySet()) {
-				System.out.println(entry.getKey() + "=" + entry.getValue());
-			}
-
-			// close reader
-			reader.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
 	}
 
 	@Override
 	public void onInitializeClient() {
-		RegisterParticlesClient();
+		LOGGER.info("Initializing Client Scribe!");
 	}
 
-	public static void RegisterParticlesClient() {
-		ParticleFactoryRegistry.getInstance().register(DATA_DRIVEN_PARTICLE, DataDrivenParticle.Factory::new);
+	public static void InitializeParticle(Identifier identifier){
+		Registry.register(Registry.PARTICLE_TYPE, identifier, PARTICLE);
+	}
+
+	public static void InitializeParticleClient(Identifier identifier, ParticleFactory factory){
+		ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).register(((atlasTexture, registry) -> {
+			registry.register(new Identifier(identifier.getNamespace(), "particle/" + identifier.getPath()));
+		}));
+
+		ParticleFactoryRegistry.getInstance().register(PARTICLE, factory);
 	}
 }
