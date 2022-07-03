11e0c7c0bd5a8ff0dbb9bc8e1117e2ba2a270b01
Outer Cloud Studio <outercloudstudio@gmail.com>
2022-07-03 01:36:21 -0500

    Cleanup Build.gradle + Debug Scribe

--- src/main/java/net/fabricmc/scribe/Scribe.java
+++ src/main/java/net/fabricmc/scribe/Scribe.java
@@ -1,21 +1,12 @@
 package net.fabricmc.scribe;
 
-import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Scribe implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+public class Scribe {
 	public static final Logger LOGGER = LoggerFactory.getLogger("scribe");
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+	public void Debug() {
 		LOGGER.info("Hello Fabric world!");
 	}
 }
